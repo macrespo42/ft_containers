25497bf90008d5b96650f1ec85137d4b05e2a47f
macrespo42 <maxime.crespopro@gmail.com>
2021-08-30 18:55:40 +0200

    add vector to ft namespace

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "vector.hpp"
+#include <vector>
 
 int main(void)
 {
+	ft::vector<int> ft;
+	std::vector<int> original;
 	return 0;
 }
--- src/vector.hpp
+++ src/vector.hpp
@@ -3,25 +3,37 @@
 
 #include <memory>
 
-template < class T, class Alloc = std::allocator<T> >
-class vector
+namespace ft
 {
-private:
-	typedef T value_type;
-	typedef Alloc allocator_type;
-	typedef typename allocator_type::reference reference;
-	typedef typename allocator_type::const_reference const_reference;
-	typedef typename allocator_type::pointer pointer;
-	typedef typename allocator_type::const_pointer const_pointer;
-	/* TO DO
-	iterator
-	const_iterator
-	reverse_iterator
-	const_reverse_iterator
-	difference_type
-	*/
-	typedef std::size_t size_type;
+	template < class T, class Alloc = std::allocator<T> >
+	class vector
+	{
+	private:
+		typedef T value_type;
+		typedef Alloc allocator_type;
+		typedef typename allocator_type::reference reference;
+		typedef typename allocator_type::const_reference const_reference;
+		typedef typename allocator_type::pointer pointer;
+		typedef typename allocator_type::const_pointer const_pointer;
+		/* TO DO
+		iterator
+		const_iterator
+		reverse_iterator
+		const_reverse_iterator
+		difference_type
+		*/
+		typedef std::size_t size_type;
 
-};
+		unsigned int _size;
+		pointer  _vector;
 
+
+	public:
+		explicit vector (const allocator_type& alloc = allocator_type())
+		{
+			this->_size = 0;
+		}
+
+	};
+}
 #endif
