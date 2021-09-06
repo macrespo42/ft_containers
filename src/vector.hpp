61d6bcfabfea83739f4a27dc1f23cb90e83ca07a
macrespo42 <maxime.crespopro@gmail.com>
2021-09-06 16:31:25 +0200

    vector range constructor

--- src/vector.hpp
+++ src/vector.hpp
@@ -47,18 +47,45 @@ namespace ft
 		 *
 		 */
 
-		explicit vector (const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _allocator(alloc)
+
+		// default
+		explicit vector(const allocator_type& alloc = allocator_type()) :
+		_size(0),
+		_capacity(0),
+		_allocator(alloc)
 		{
 			this->_vector = this->_allocator.allocate(0);
 		}
 
-		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : 
+		// fill
+		explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : 
 		_size(n),
 		_capacity(n),
 		_allocator(alloc)
 		{
 			this->_vector = this->_allocator.allocate(this->_capacity);
 			this->_allocator.construct(this->_vector, val);
+		}
+
+		// range
+		template <class InputIterator>
+		vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()):
+		_allocator(alloc)
+		{
+			InputIterator first_cpy = first;
+			int index = 0;
+
+			for (size_type iteratorDiff = 0; firstCpy != last; iteratorDiff++)
+				firstCpy++;
+			this->_size = iteratorDiff;
+			this->_capacity = iteratorDiff;
+			this->_vector = this->_allocator.allocate(this->_capacity);
+			while (first != last)
+			{
+				this->_vector[index] = *first;
+				first++;
+				index++; 
+			}
 		}
 	};
 }
