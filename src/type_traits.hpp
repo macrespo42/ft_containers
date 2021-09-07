a7370d6edb0d0c3455a9e98256d49ab1e6503629
macrespo42 <maxime.crespopro@gmail.com>
2021-09-07 15:14:20 +0200

    is_integral done with all c++98 needed specialisations

--- src/type_traits.hpp
+++ src/type_traits.hpp
@@ -8,6 +8,49 @@ namespace ft
 
 	template <class T>
 	struct enable_if<true, T> { typedef T type; };
+
+	template <class T>
+	struct is_integral { static const bool value = false; };
+
+	template <>
+	struct is_integral<bool> { static const bool value = true; };
+
+	template <>
+	struct is_integral<char> { static const bool value = true; };
+
+	template <>
+	struct is_integral<wchar_t> { static const bool value = true; };
+
+	template <>
+	struct is_integral<signed char> { static const bool value = true; };
+
+	template <>
+	struct is_integral<short int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<long int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<long long int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned char> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned short int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned long int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned long long int> { static const bool value = true; };
+
 }
 
 #endif
