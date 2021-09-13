92b2bc1d6a7817c986a50e0790321cc10a4a3e0d
macrespo42 <maxime.crespopro@gmail.com>
2021-09-13 17:39:13 +0200

    vector constructors unite test

--- main.cpp
+++ main.cpp
@@ -4,8 +4,26 @@
 #include <iostream>
 #include <vector>
 
-#define NAMESPACE ft
-// #define NAMESPACE std
+// #define NAMESPACE ft
+#define NAMESPACE std
+
+void vectorConstructor_test(void)
+{
+  // constructors used in the same order as described above:
+  NAMESPACE::vector<int> first;                                // empty vector of ints
+  NAMESPACE::vector<int> second (4,100);                       // four ints with value 100
+  NAMESPACE::vector<int> third (second.begin(),second.end());  // iterating through second
+  NAMESPACE::vector<int> fourth (third);                       // a copy of third
+
+  // the iterator constructor can also be used to construct from arrays:
+  int myints[] = {16,2,77,29};
+  NAMESPACE::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+
+  std::cout << "The contents of fifth are:";
+  for (NAMESPACE::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+}
 
 void rendRbegin_test(void)
 {
@@ -25,6 +43,7 @@ void rendRbegin_test(void)
 
 int main ()
 {
+  vectorConstructor_test();
   rendRbegin_test();
   return 0;
 }
