e9e5d546f4eceda5de1d81d9e3b101c1901b2d91
macrespo42 <maxime.crespopro@gmail.com>
2021-09-24 10:05:10 +0200

    reindent algo

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -11,30 +11,30 @@ namespace ft
 	}
 }
 
-template <class InputIterator1, class InputIterator2>
-bool
-equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
-{
-	while (first1!=last1)
+	template <class InputIterator1, class InputIterator2>
+	bool
+	equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
 	{
-	    if (!(*first1 == *first2))
-	    	return false;
-	    ++first1; ++first2;
+		while (first1!=last1)
+		{
+		    if (!(*first1 == *first2))
+		    	return false;
+		    ++first1; ++first2;
+		}
+		return true;
 	}
-	return true;
-}
 
-template <class InputIterator1, class InputIterator2>
-bool
-lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
-{
-	while (first1!=last1)
+	template <class InputIterator1, class InputIterator2>
+	bool
+	lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
 	{
-		if (first2==last2 || *first2<*first1) return false;
-		else if (*first1<*first2) return true;
-		++first1; ++first2;
+		while (first1!=last1)
+		{
+			if (first2==last2 || *first2<*first1) return false;
+			else if (*first1<*first2) return true;
+			++first1; ++first2;
+		}
+		return (first2!=last2);
 	}
-	return (first2!=last2);
-}
 
 #endif
