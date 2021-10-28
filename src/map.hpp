7267f6c79accfc6c11605e179bda34fa47ce7275
macrespo42 <maxime.crespopro@gmail.com>
2021-10-28 17:39:32 +0200

    map typedef + private members

--- src/map.hpp
+++ src/map.hpp
@@ -13,7 +13,25 @@ namespace ft
     template <class Key, class T, class compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key, T> > >
     class map
     {
+        public:
 
+        typedef ft::pair<const Key, T> value_type;
+        typedef Key key_type;
+        typedef T mapped_type;
+        typedef compare key_compare;
+        // value_compare;
+        typedef Alloc allocator_type;
+        typedef allocator_type::reference reference;
+        typedef allocator_type::const_reference const_reference;
+        typedef allocator_type::pointer pointer;
+        typedef allocator_type::const_pointer pointer;
+        typedef size_t size_type;
+
+        private:
+
+        size_type _size;
+        allocator_type _allocator;
+        ft::rb_tree<value_type> _map;
     };
 }
 
