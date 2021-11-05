84915479e8bc957a5086115cc4b104a206ff7a95
macrespo42 <maxime.crespopro@gmail.com>
2021-11-05 13:14:10 +0100

    map rbegin

--- src/map.hpp
+++ src/map.hpp
@@ -100,6 +100,15 @@ namespace ft
             return (const_iterator(_map.get_nil_node()));
         }
 
+        reverse_iterator rbegin()
+        {
+            return (reverse_iterator(_map.get_nil_node()));
+        }
+
+        const_reverse_iterator rbegin() const
+        {
+            return (const_reverse_iterator(_map.get_nil_node()));
+        }
     };
 }
 
