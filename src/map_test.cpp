25318b1096a61972a451a7cd102a2c4d7137d8a7
macrespo42 <maxime.crespopro@gmail.com>
2021-11-10 02:51:36 +0100

    max_size test ok

--- src/map_test.cpp
+++ src/map_test.cpp
@@ -110,6 +110,21 @@ int size_test(void)
   return 0;
 }
 
+int max_size_test(void)
+{
+  int i;
+  ft::map<int,int> mymap;
+
+  if (mymap.max_size()>1000)
+  {
+    for (i=0; i<1000; i++) mymap[i]=0;
+    std::cout << "The map contains 1000 elements.\n";
+  }
+  else std::cout << "The map could not hold 1000 elements.\n";
+
+  return 0;
+}
+
 int main(void)
 {
     // constructors_test();
@@ -118,5 +133,6 @@ int main(void)
     // rbegin_rend_test();
     // empty_test();
     // size_test();
+    max_size_test();
     return 0;
 }
